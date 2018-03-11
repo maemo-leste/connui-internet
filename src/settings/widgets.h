d6cfd229f0e2b377fb242da93eab8ace3bceca41
Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>
2018-03-11 18:45:51 +0200

    make iap_widgets_insert_text_maxval_reach public
    Signed-off-by: Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>

--- src/settings/widgets.h
+++ src/settings/widgets.h
@@ -6,13 +6,16 @@ void
 iap_widgets_insert_only_ascii_text(GtkEditable *editable, gchar *new_text,
                                    gint new_text_length, gpointer position,
                                    gpointer user_data);
-
 void
 iap_widgets_insert_text_no_8bit_maxval_reach(GtkEditable *editable,
                                              gchar *new_text,
                                              gint new_text_length,
                                              gpointer position,
                                              gpointer user_data);
+void
+iap_widgets_insert_text_maxval_reach(GtkEditable *editable, gchar *new_text,
+                                     gint new_text_length, gpointer position,
+                                     gpointer user_data);
 
 /* static widgets */
 GtkWidget *iap_widgets_create_static_picker_button(const gchar *title,
